8c57768d401ba8bb8f543d89f436a442e702befb
ekenes2 <ekenes2@huskers.unl.edu>
2022-03-28 14:25:25 -0500

    Completed top nav bar

--- src/State.js
+++
@@ -1,8 +0,0 @@
-import React, {useState} from 'react'
-
-const[wid, setWid]= useState('0%');
-<button onClick={openSidenav}>Open</button>
-const openSidenav = ( ) => {
-    setWid('25%')
- }
-<button onClick={props.closeNav}>X</button>
