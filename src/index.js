d28d684ed5c02ecddc29326739beffabf1e0fa21
hannahpokharel <69911614+hannahpokharel@users.noreply.github.com>
2022-03-28 00:25:22 -0500

    added log in page

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import styled from "@emotion/styled";
 import './index.css';
 import App from './App';
+import "./pages/LogInScreen";
 import reportWebVitals from './reportWebVitals';
+import LogInScreen from './App';
+
+export { default as LogInScreen } from "./LogInScreen";
 
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <LogInScreen/>,
   document.getElementById('root')
 );
+
+
+
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
