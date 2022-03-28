0112dd47c7083cd2f3076baa3e8857fe1b9cc2d9
ekenes2 <ekenes2@huskers.unl.edu>
2022-03-28 14:12:02 -0500

    Update app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
-import SideNav from './SideNav';
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from './pages';
+import About from './pages/about';
+import Contact from './pages/contact';
+import SignUp from './pages/signup';
+import SignIn from './pages/signin';
 
-
-
-function App(props, state) {
+function App() {
   return (
-    <div className='container'>
-      <SideNav name='User' width={wid} closeNav={closeSidenav}/>
-    </div>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/signin" component={SignIn} />
+        <Route path="/sign-up" component={SignUp} />
+      </Switch>
+    </Router>
   );
 }
 
