a03eb9c217f47c352bc522d276d8a4a384004840
teju boinpally <70039439+tboinpally@users.noreply.github.com>
2022-03-31 14:54:34 -0500

    Working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
-import React, { Component }  from 'react';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Home from './pages/Home'
-import Reports from './pages/Reports'
-import Products from './pages/Products'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Reports from './pages/Reports';
+import Products from './pages/Products';
+
 
 function App() {
   return (
@@ -12,9 +13,9 @@ function App() {
     <Router>
        <Navbar />
        <Routes>
-         <Route path='/' exact component={Home} />
-         <Route path='/reports' component={Reports} />
-         <Route path='/products' component={Products} />
+         <Route path='/reports' element={< Reports />} />
+         <Route path='/products' element={< Products />} />
+         <Route exact path='/' element={<Home />} />
        </Routes>
     </Router>
  
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,15 +16,13 @@ function Navbar() {
     <IconContext.Provider value={{ color: '#fff' }}>
         <div className="navbar">
             <Link to="#" className='menu-bars'>
-              <FaIcons.FaBars onClick={showSidebar}/> 
+              <FaIcons.FaBars/> 
             </Link>
         </div>
         <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
-            <ul className='nav-menu-items' onClick={showSidebar}>
+            <ul className='nav-menu-items' >
                 <li className="navbar-toggle">
-                    <Link to='#' className='menu-bars'>
-                        <AiIcons.AiOutlineClose />
-                    </Link>
+                    <a className='title'>Uplft</a>
                 </li>
                 {SidebarData.map((item, index) => {
                     return (
