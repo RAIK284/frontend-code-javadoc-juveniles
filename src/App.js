f9f83953f188feef100acf60bd2191278d9ed8f9
teju boinpally <70039439+tboinpally@users.noreply.github.com>
2022-03-31 18:10:30 -0500

    this should work

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Messages from './pages/Messages';
+import Leaderboard from './pages/Leaderboard';
+import Shop from './pages/Shop';
+import Profile from './pages/Profile.js';
+
+
+function App() {
+  return (
+    <>
+      <Router>
+       <Navbar />
+       <Routes>
+         <Route path='/messages' element={< Messages />} />
+         <Route path='/leaderboard' element={< Leaderboard />} />
+         <Route path='/shop' element={< Shop />} />
+         <Route path='/profile' element={< Profile />} />
+         <Route exact path='/' element={<Home />} />
+       </Routes>
+    </Router>
+ 
+    </>
+  );
+}
+
+export default App;
