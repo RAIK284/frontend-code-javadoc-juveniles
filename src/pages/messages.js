815dec5b02fbb169502bca7b81a3ca6a2ef4052d
hannahpokharel <69911614+hannahpokharel@users.noreply.github.com>
2022-03-31 15:19:41 -0500

    Home page with top navbar

---
+++ src/pages/messages.js
@@ -0,0 +1,20 @@
+import { render } from "@testing-library/react";
+import React from "react";
+
+const Messages = () => {
+        return (
+            <div
+                style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    height: '100vh'
+                }}
+            >
+                <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem, totam.</h1>
+            </div>
+        );
+    }; 
+
+
+export default Messages;
