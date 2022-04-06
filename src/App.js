218738e1a22b4e97c9a05f3ffa7f48d08795935d
ekenes2 <ekenes2@huskers.unl.edu>
2022-04-06 15:26:46 -0500

    App

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useContext}  from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
@@ -7,24 +7,17 @@ import Messages from './pages/Messages';
 import Leaderboard from './pages/Leaderboard';
 import Shop from './pages/Shop';
 import Profile from './pages/Profile.js';
+import SignUp from './pages/LoginPage/SignUp';
+import SignIn from './pages/LoginPage/SignIn'
+import PasswordReset from './pages/LoginPage/PasswordReset';
+import {UserContext} from './pages/LoginPage/UserProvider';
+import Application from './pages/LoginPage/Application';
 
 
 function App() {
-  return (
-    <>
-      <Router>
-       <Navbar />
-       <Routes>
-         <Route path='/messages' element={< Messages />} />
-         <Route path='/leaderboard' element={< Leaderboard />} />
-         <Route path='/shop' element={< Shop />} />
-         <Route path='/profile' element={< Profile />} />
-         <Route exact path='/' element={<Home />} />
-       </Routes>
-    </Router>
- 
-    </>
-  );
+  <UserContext>
+    <Application />
+  </UserContext>
 }
 
 export default App;
