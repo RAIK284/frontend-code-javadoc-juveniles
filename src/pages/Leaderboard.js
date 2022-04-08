8b48623b8d0399c474b8680ab7835cf8ddbbd211
mradlicz2 <mradlicz2@huskers.unl.edu>
2022-04-08 02:31:59 -0500

    Added coin and xp leaderboard from backend

--- src/pages/Leaderboard.js
+++ src/pages/Leaderboard.js
@@ -1,165 +1,119 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
-import { PageContents, RootWrapperLeaderboard, WelcomeText, Trophies, YourTrophies, TrophyBox, MessagesTable, TableHeader,
-    SenderBox, TimeBox, SenderText, TimeText, SubjectBox, MessageBox, SubjectText, MessageText,
-    CoinsGainedBox, CoinsGainedText, MessageRow, SenderBox_0001, TimeBox_0001, SenderText_0001, TimeText_0001,
-    SubjectBox_0001, MessageBox_0001, MessageText_0001, SubjectText_0001, CoinBox, CoinAmount,
-    CoinIcon, CoinIcon_0001, CoinBgImage, CoinNumber, Background, WhiteBackground, ContentBody, Messages, RecentMessages,
-    StatBar, ProfilePic, ProfileImage, ProfileImage_0001, ProfileImage_0002, Username, Xp, XpImage, XpImage_0001,
-    XpBigImage, Coins, CoinImage, CoinImage_0001, CoinBgImage_0001, CoinAmount_0001, LeaderboardHeader, LeaderboardTabs, 
-    CoinsEarnedTab, Underline, Underline_0001, AllTimeCoinsEarned, AllTimeXpUsed, XpUsedTab, _000 } from './PageElements';
+import {
+  PageContents,
+  ContentBody,
+  LeaderboardHeader,
+  LeaderboardTabs,
+  CoinsEarnedTab,
+  Underline,
+  Underline_0001,
+  AllTimeCoinsEarned,
+  AllTimeXpUsed,
+  XpUsedTab,
+} from "./PageElements";
+
+import fetch from "node-fetch";
 
 import "../App.css";
 
-const data = [
-  { position: 1, user: "@charlidamelio", all_time_coins_earned: 32435},
-  { position: 2, user: "@bingbong", all_time_coins_earned: 32435},
-  { position: 3, user: "@bigtimerush", all_time_coins_earned: 32435},
-  { position: 4, user: "@beep_boop", all_time_coins_earned: 32435},
-  { position: 5, user: "@dixie_damelio", all_time_coins_earned: 32435},
-  { position: 6, user: "@millenial2323", all_time_coins_earned: 32435},
-  { position: 7, user: "@that_cool_guy", all_time_coins_earned: 32435},
-  { position: 8, user: "@jsadfaj", all_time_coins_earned: 32435},
-  { position: 9, user: "@akjdsfa;lkdsfa", all_time_coins_earned: 32435},
-  { position: 10, user: "@kjdshfakjhd", all_time_coins_earned: 32435},
-]
-
 function Leaderboard() {
-  return (
-    <PageContents>
-      <div className='bluebackground'>
-        <div className='whitebackground'>
-        </div>
-      </div>
-  <ContentBody>
-      <LeaderboardHeader>
-        Leaderboard
-      </LeaderboardHeader>
-      <LeaderboardTabs>
-        <CoinsEarnedTab>
-          <Underline/>
-        <AllTimeCoinsEarned>
-            ALL-TIME COINS EARNED
-          </AllTimeCoinsEarned>
+  const [data, setData] = useState(null);
+  const [isCoin, setIsCoin] = useState(true);
+
+  useEffect(() => {
+    async function fetchData() {
+      let response = null;
+      if (isCoin) {
+        response = await fetch(
+          "https://us-central1-uplft-9ed97.cloudfunctions.net/app/getCoinLeaderboard"
+        );
+      } else {
+        response = await fetch(
+          "https://us-central1-uplft-9ed97.cloudfunctions.net/app/getXpLeaderboard"
+        );
+      }
+      const json = await response.json();
+      setData(json);
+    }
+    fetchData();
+  }, []);
+
+  function generateTable() {
+    return (
+      <tbody>
+        <tr>
+          <th>Rank</th>
+          <th>User</th>
+          <th>{isCoin ? "Total Coins Earned" : "Total XP Used"}</th>
+        </tr>
+        {data.slice(0, 10).map((val, key) => {
+          return (
+            <tr key={key}>
+              <td>{key + 1}</td>
+              <td>{val.username}</td>
+              <td>{isCoin ? val.totalCoins : val.xpUsed}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    );
+  }
+
+  function getCoinButton() {
+    if (isCoin) {
+      return (
+        <CoinsEarnedTab onClick={() => setIsCoin(true)}>
+          <Underline />
+          <AllTimeCoinsEarned>ALL-TIME COINS EARNED</AllTimeCoinsEarned>
         </CoinsEarnedTab>
-      <XpUsedTab>
-          <Underline_0001 xmlns="http://www.w3.org/2000/svg">
-            <path fill="rgb(128, 128, 128)" d="M 0 0 L 169 0 L 169 3 L 0 3 L 0 0 Z"/>
-          </Underline_0001>
-        <AllTimeXpUsed>
-            ALL-TIME XP USED
-          </AllTimeXpUsed>
+      );
+    }
+    return (
+      <CoinsEarnedTab onClick={() => setIsCoin(true)}>
+        <Underline style={{ backgroundColor: "rgb(128, 128, 128)" }} />
+        <AllTimeXpUsed>ALL-TIME COINS EARNED</AllTimeXpUsed>
+      </CoinsEarnedTab>
+    );
+  }
+
+  function getXpButton() {
+    if (isCoin) {
+      return (
+        <XpUsedTab onClick={() => setIsCoin(false)}>
+          <Underline_0001 />
+          <AllTimeXpUsed>ALL-TIME XP USED</AllTimeXpUsed>
         </XpUsedTab>
-      </LeaderboardTabs>
-      <table_leaderboard>
-                    <tr>
-                      <th>Position</th>
-                      <th>User</th>
-                      <th>Total Coins Earned</th>
-                    </tr>
-                    {data.map((val, key) => {
-                      return (
-                        <tr key={key}>
-                          <td>{val.position}</td>
-                          <td>{val.user}</td>
-                          <td>{val.all_time_coins_earned}</td>
-                        </tr>
-                      )
-                    })}
-              </table_leaderboard>  
-    {/* <Table>
-        <RecentMessages>
-          Recent Messages
-        </RecentMessages>
-      <MessagesTable>
-          <TableHeader>
-            <SenderBox/>
-          <TimeBox/>
-          <SenderText>
-              Sender
-            </SenderText>
-          <TimeText>
-              Time Received
-            </TimeText>
-          <SubjectBox/>
-          <MessageBox/>
-          <SubjectText>
-              Subject
-            </SubjectText>
-          <MessageText>
-              Message
-            </MessageText>
-          <CoinsGainedBox/>
-          <CoinsGainedText>
-              Coins Gained
-            </CoinsGainedText>
-          </TableHeader>
-        <MessageRow>
-            <SenderBox_0001/>
-          <TimeBox_0001/>
-          <SenderText_0001>
-              @kevinhart56
-            </SenderText_0001>
-          <TimeText_0001>
-              02/12/2022 4:36pm
-            </TimeText_0001>
-          <SubjectBox_0001/>
-          <MessageBox_0001/>
-          <MessageText_0001>
-              Have a great day man, you are the best! 😊 💯
-            </MessageText_0001>
-          <SubjectText_0001>
-              I love you bro!
-            </SubjectText_0001>
-          <CoinBox/>
-          <CoinAmount>
-              <CoinIcon>
-                <CoinIcon_0001>
-                  <CoinBgImage src="grida://assets-reservation/images/270:511" alt="image of CoinBgImage"/>
-                </CoinIcon_0001>
-              </CoinIcon>
-            <CoinNumber>
-                103
-              </CoinNumber>
-            </CoinAmount>
-          </MessageRow>
-        </MessagesTable>
-      </Table> */}
-    </ContentBody>
-  <StatBar>
-      <ProfilePic>
-        <ProfileImage>
-          <ProfileImage_0001>
-            <ProfileImage_0002 src="https://www.biography.com/.image/t_share/MTgwOTI0NDYwNjQ2Mjc4MjMy/gettyimages-1061959920.jpg" alt="image of ProfileImage"/>
-          </ProfileImage_0001>
-        </ProfileImage>
-      <Username>
-          @username
-        </Username>
-      </ProfilePic>
-    <Xp>
-        <XpImage>
-          <XpImage_0001>
-            <XpBigImage src="https://image.shutterstock.com/image-vector/vector-icon-gold-achievement-xp-260nw-1151064896.jpg" alt="image of XpBigImage"/>
-          </XpImage_0001>
-        </XpImage>
-      <_000>
-          000
-        </_000>
-      </Xp>
-    <Coins>
-        <CoinImage>
-          <CoinImage_0001>
-            <CoinBgImage_0001 src="https://image.shutterstock.com/image-vector/vector-money-icon-gold-coin-260nw-1138554755.jpg" alt="image of CoinBgImage"/>
-          </CoinImage_0001>
-        </CoinImage>
-      <CoinAmount_0001>
-          000
-        </CoinAmount_0001>
-      </Coins>
-    </StatBar>
-  </PageContents>
-  );
+      );
+    }
+    return (
+      <XpUsedTab onClick={() => setIsCoin(false)}>
+        <Underline_0001 style={{ backgroundColor: "rgb(24, 120, 208)" }} />
+        <AllTimeCoinsEarned>ALL-TIME XP USED</AllTimeCoinsEarned>
+      </XpUsedTab>
+    );
+  }
+
+  if (data) {
+    return (
+      <PageContents>
+        <div className="bluebackground">
+          <div className="whitebackground"></div>
+        </div>
+        <ContentBody>
+          <LeaderboardHeader>Leaderboard</LeaderboardHeader>
+          <LeaderboardTabs>
+            {getCoinButton()}
+
+            <XpUsedTab onClick={() => setIsCoin(false)}>
+              {getXpButton()}
+            </XpUsedTab>
+          </LeaderboardTabs>
+          <table_leaderboard>{generateTable()}</table_leaderboard>
+        </ContentBody>
+      </PageContents>
+    );
+  }
 }
 
 export default Leaderboard;
