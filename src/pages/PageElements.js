be79b7134bab526b4237cf2bf0e03ed9f64b67b9
mradlicz2 <mradlicz2@huskers.unl.edu>
2022-05-05 14:20:23 -0500

    The ultimate fix

--- src/pages/PageElements.js
+++ src/pages/PageElements.js
@@ -3,7 +3,7 @@ import styled from "@emotion/styled";
 export const PageContents = styled.div`
 	position: relative;
     left: 264px;
-    width: 100vw;
+    width: 70vw;
 	height: 1024px;
     background-color: rgba(194, 229, 255, 1); `;
 
