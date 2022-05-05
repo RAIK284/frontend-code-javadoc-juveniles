ead2684092e22f41415750b77fc322008ca9a291
mradlicz2 <mradlicz2@huskers.unl.edu>
2022-05-05 14:24:29 -0500

    i lied people died

--- src/pages/PageElements.js
+++ src/pages/PageElements.js
@@ -3,7 +3,7 @@ import styled from "@emotion/styled";
 export const PageContents = styled.div`
 	position: relative;
     left: 264px;
-    width: 70vw;
+    width: 100vw;
 	height: 1024px;
     background-color: rgba(194, 229, 255, 1); `;
 
