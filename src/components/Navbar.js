2d5e8627ef458298fda624fa0942b9382bbb02db
teju boinpally <70039439+tboinpally@users.noreply.github.com>
2022-05-06 18:31:46 -0500

    done commenting

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,17 +5,21 @@ import './Navbar.css';
 import { IconContext } from 'react-icons';
 import "@fontsource/comfortaa";
 
+/* this sets the navigation bar to true in the use state, so it appears on all the pages and this file 
+controls the navigation bar and its components*/
 function Navbar() {
     const [sidebar] = useState(true)
-    
+
     return (
         <>
+            {/* this makes all the icons white, so it is uniform */}
             <IconContext.Provider value={{ color: '#ffff' }}>
                 <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
                     <ul className='nav-menu-items' >
                         <li>
-                            <a className='title'>Uplft</a>
+                            <a className='titlepage'>Uplft</a>
                         </li>
+                        {/* this maps out the naviagtion bar linking it to the name, path, and icon data */}
                         {SidebarData.map((item, index) => {
                             return (
                                 <li key={index} className={item.cName}>
